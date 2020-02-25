21652022095c293f78c122accd22447d998713e9
Bohdan <trojanj@users.noreply.github.com>
2020-02-25 20:47:33 +0200

    Modified second app version

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 function Error(props) {
-  console.log(props.errorData);
   return (
     <div style={{color: 'red', fontSize: '36px', padding: '20px'}}>
-      {props.errorData ? `Error: ${props.errorData.message}` : 'Error: Data not received'}
+      {props.errorData ? `Error: ${props.errorData.message}` : null}
     </div>
   )
 }
